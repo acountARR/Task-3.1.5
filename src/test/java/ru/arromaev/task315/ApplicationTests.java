2465902b9da08458cb250a844ed5394809dcb3b2
Arr <Arromaev@gmail.com>
2023-06-07 11:44:45 +0500

    Загрузка master ветки

---
+++ src/test/java/ru/arromaev/task315/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arromaev.task315;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
