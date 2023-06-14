f4b35d993482fa02df7f825f4093928e85f05b32
Arr <Arromaev@gmail.com>
2023-06-14 11:16:37 +0500

    Приложение с Rest-template, взаимодействует с помощью него с открытым Rest API.

---
+++ src/main/java/ru/arromaev/task315/Communication.java
@@ -0,0 +1,65 @@
+package ru.arromaev.task315;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import ru.arromaev.task315.entity.User;
+
+
+@Component
+public class Communication {
+    private final RestTemplate restTemplate;
+    private final HttpHeaders headers;
+    private final String URL = "http://94.198.50.185:7081/api/users";
+
+    @Autowired
+    public Communication(RestTemplate restTemplate, HttpHeaders headers) {
+        this.restTemplate = restTemplate;
+        this.headers = headers;
+        //получение куки
+        this.headers.set("Cookie", String.join(";", restTemplate.headForHeaders(URL).get("Set-Cookie")));
+    }
+
+    public String getAnswer() {
+        return addUser().getBody() + " - " + updateUser().getBody() + " - " + deleteUser().getBody();
+    }
+
+    // Получение всех пользователей -  …/api/users ( GET )
+    public List<User> getAllUsers() {
+        ResponseEntity<List<User>> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {
+        });
+        return responseEntity.getBody();
+    }
+
+    // Добавление пользователя - …/api/users ( POST )
+    public ResponseEntity<String> addUser() {
+        User user = new User(3L, "James", "Brown", (byte) 5);
+        HttpEntity<User> entity = new HttpEntity<>(user, headers);
+        return restTemplate.postForEntity(URL, entity, String.class);
+    }
+
+    // Изменение пользователя - …/api/users ( PUT )
+    public ResponseEntity<String> updateUser() {
+        User user = new User(3L, "Thomas", "Shelby", (byte) 5);
+        HttpEntity<User> entity = new HttpEntity<>(user, headers);
+        return restTemplate.exchange(URL, HttpMethod.PUT, entity, String.class, 3);
+    }
+
+    // Удаление пользователя - …/api/users /{id} ( DELETE )
+    public ResponseEntity<String> deleteUser() {
+        Map<String, Long> uriVariables = new HashMap<>() {{
+            put("id", 3L);
+        }};
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        return restTemplate.exchange(URL + "/{id}", HttpMethod.DELETE, entity, String.class, uriVariables);
+    }
+
+
+
+
+}
